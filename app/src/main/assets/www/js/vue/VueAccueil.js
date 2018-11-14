7304d8f10fb4b089e9d18457c1da615da9579368
Florent F <florent.fli@gmail.com>
2018-11-14 09:46:24 -0500

    refactor position dossier css

--- app/src/main/assets/www/js/vue/VueAccueil.js
+++ app/src/main/assets/www/js/vue/VueAccueil.js
@@ -6,20 +6,58 @@ var VueAccueil = (function () {
 
         this.afficher = function () {
             document.getElementsByTagName("body")[0].innerHTML = pageAccueil;
+
             var granimInstance = new Granim({
-                element: '#canvas-basic',
-                direction: 'left-right',
+                element: '#canvas-interactive',
+                name: 'interactive-gradient',
+                elToSetClassOn: '.canvas-interactive-wrapper',
+                direction: 'diagonal',
                 isPausedWhenNotInView: true,
+                stateTransitionSpeed: 500,
                 states : {
                     "default-state": {
                         gradients: [
-                            ['#ff9966', '#ff5e62'],
-                            ['#00F260', '#0575E6'],
-                            ['#e1eec3', '#f05053']
-                        ]
+                            ['#B3FFAB', '#12FFF7'],
+                            ['#ADD100', '#7B920A'],
+                            ['#1A2980', '#26D0CE']
+                        ],
+                        transitionSpeed: 10000
+                    },
+                    "violet-state": {
+                        gradients: [
+                            ['#9D50BB', '#6E48AA'],
+                            ['#4776E6', '#8E54E9']
+                        ],
+                        transitionSpeed: 2000
+                    },
+                    "orange-state": {
+                        gradients: [ ['#FF4E50', '#F9D423'] ],
+                        loop: false
                     }
                 }
             });
+
+// With jQuery
+            $('#default-state-cta').on('click', function(event) {
+                event.preventDefault();
+                granimInstance.changeState('default-state');
+                setClass('#default-state-cta')
+            });
+            $('#violet-state-cta').on('click', function(event) {
+                event.preventDefault();
+                granimInstance.changeState('violet-state');
+                setClass('#violet-state-cta')
+            });
+            $('#orange-state-cta').on('click', function(event) {
+                event.preventDefault();
+                granimInstance.changeState('orange-state');
+                setClass('#orange-state-cta')
+            });
+
+            function setClass(element) {
+                $('.canvas-interactive-wrapper a').removeClass('active');
+                $(element).addClass('active');
+            };
         };
     }
 
