733da5cf73c196fdef4a50628ef2315fbca3ece6
Florent F <florent.fli@gmail.com>
2018-11-20 11:50:09 -0500

    formulaire creer compte

--- app/src/main/assets/www/js/accesseur/UtilisateurDAO.js
+++ app/src/main/assets/www/js/accesseur/UtilisateurDAO.js
@@ -1,21 +1,37 @@
 var UtilisateurDAO = (function () {
 
     return function () {
-        this.requete = function() {
+        this.requete = function () {
             var url = 'http://localhost:8080/connexion';
             var data = {pseudo: 'flo', mot_de_passe: 'password'};
 
             fetch(url, {
                 method: 'POST', // or 'PUT'
                 body: JSON.stringify(data), // data can be `string` or {object}!
-                headers:{
+                headers: {
                     'authentification': 'paul',
                     'Content-Type': 'application/json',
                     'Accept': 'application/json'
                 }
-            }).then(res => res.json())
-                .then(response => console.log('Success:', JSON.stringify(response)))
-                .catch(error => console.error('Error:', error));
+            }).then(res => {
+                    res.json();
+                    console.log(res);
+                    console.log(res.status);
+                    console.log(res.body);
+
+                }
+            )
+            .then(response => {
+                    console.log('Success:', JSON.stringify(response));
+                }
+            )
+                .catch(error => {
+                        console.error('Error:', error);
+                        console.log('erersd');
+                    }
+                );
+
+
         }
     }
 })();
