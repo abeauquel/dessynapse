f57c4efeecad6cbc818ad57de07490af96a627e5
Florent F <florent.fli@gmail.com>
2018-11-20 11:28:05 -0500

    connexion fonctionnelle (preuve de concept)

--- app/src/main/assets/www/js/accesseur/UtilisateurDAO.js
+++ app/src/main/assets/www/js/accesseur/UtilisateurDAO.js
@@ -1,29 +1,21 @@
 var UtilisateurDAO = (function () {
 
     return function () {
-        this.requete = function post(path, params, method) {
+        this.requete = function() {
+            var url = 'http://localhost:8080/connexion';
+            var data = {pseudo: 'flo', mot_de_passe: 'password'};
 
-            method = method || "post"; // Set method to post by default if not specified.
-
-            // The rest of this code assumes you are not using a library.
-            // It can be made less wordy if you use one.
-            var form = document.createElement("form");
-            form.setAttribute("method", method);
-            form.setAttribute("action", path);
-
-            for (var key in params) {
-                if (params.hasOwnProperty(key)) {
-                    var hiddenField = document.createElement("input");
-                    hiddenField.setAttribute("type", "hidden");
-                    hiddenField.setAttribute("name", key);
-                    hiddenField.setAttribute("value", params[key]);
-
-                    form.appendChild(hiddenField);
+            fetch(url, {
+                method: 'POST', // or 'PUT'
+                body: JSON.stringify(data), // data can be `string` or {object}!
+                headers:{
+                    'authentification': 'paul',
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
                 }
-            }
-
-            document.body.appendChild(form);
-            console.log(form.submit());
+            }).then(res => res.json())
+                .then(response => console.log('Success:', JSON.stringify(response)))
+                .catch(error => console.error('Error:', error));
         }
     }
 })();
