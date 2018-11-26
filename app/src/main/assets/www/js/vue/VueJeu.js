0528b3a1c5b72ab167f30f2f3f5a1f5bb8d8cc83
paul <jacobs.paul.pro@gmail.com>
2018-11-25 23:54:09 -0500

    Feat (Dessiner) : ajout de la compatibilitee tactile

--- app/src/main/assets/www/js/vue/VueJeu.js
+++ app/src/main/assets/www/js/vue/VueJeu.js
@@ -2,7 +2,7 @@ var VueJeu = (function () {
     var pagejeu = document.getElementById("page-jeu").innerHTML;
     
     var ancienX, ancienY, nouveauX, nouveauY = 0;
-    var canvas, contexte = false;
+    var canvas, contexte, click, appuie = false;
 
     return function (actionNavigationConnexion) {
 
@@ -14,10 +14,26 @@ var VueJeu = (function () {
 
     function initialiser(){
 	canvas = document.getElementById("canvas-jeu");
+	document.body.addEventListener('touchmove', function(e) { e.preventDefault(); }, false);
 	contexte = canvas.getContext("2d");
 
-	canvas.addEventListener("mousemove", function (e){
+	/*canvas.addEventListener("mousemove", function (e){
 		recupererPosition('deplacement',e)
+	},false);*/
+
+	canvas.addEventListener("touchstart", function (e){
+	    //e.preventDefault();
+		recupererPosition('toucher_on',e)
+	},false);
+
+	canvas.addEventListener("touchstop", function (e){
+	    //e.preventDefault();
+		recupererPosition('toucher_off',e)
+	},false);
+
+	canvas.addEventListener("touchmove", function (e){
+	    e.preventDefault();
+		recupererPosition('toucher_deplacement',e)
 	},false);
 	
     }
@@ -30,15 +46,33 @@ var VueJeu = (function () {
 		nouveauY = e.clientY - canvas.offsetTop;
 		dessiner();
 	}
+
+	if (evenement = "toucher_on"){
+        appuie = true;
+        //nouveauX = e.touches[0].clientX;// - canvas.offsetLeft;
+        //nouveauY = e.touches[0].clientY;// - canvas.offsetTop;
+	}
+
+	if (evenement = "toucher_off"){
+	    console.log("toucher off");
+        appuie = false;
+	}
+
+    if (evenement = "toucher_deplacement")
+        ancienX = nouveauX;
+        ancienY = nouveauY;
+        nouveauX = e.touches[0].clientX;// - canvas.offsetLeft;
+        nouveauY = e.touches[0].clientY;// - canvas.offsetTop;
+        dessiner();
     }
 
     function dessiner(){
-	console.log(ancienX, ancienY);
+    console.log(""+ancienX+ancienY+nouveauX+nouveauY);
 	contexte.beginPath();
 	contexte.moveTo(ancienX, ancienY);
 	contexte.lineTo(nouveauX, nouveauY);
-        contexte.strokeStyle = 2;
-        contexte.lineWidth = "black";
+    contexte.strokeStyle = 2;
+    contexte.lineWidth = "blue";
 	contexte.stroke();
 	contexte.closePath();
     }
