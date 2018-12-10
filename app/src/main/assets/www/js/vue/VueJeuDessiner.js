143af8014f4db6cdabbc4dd1328308c1698ab7ee
paul <jacobs.paul.pro@gmail.com>
2018-12-10 17:27:45 -0500

    Feat (Lib) : utilisation de la librairie create js

--- app/src/main/assets/www/js/vue/VueJeuDessiner.js
+++ app/src/main/assets/www/js/vue/VueJeuDessiner.js
@@ -1,103 +1,93 @@
 var VueJeuDessiner = (function () {
-    var pagejeu = document.getElementById("page-jeu").innerHTML;
 
-    var ancienX, ancienY, nouveauX, nouveauY = 0;
-    var canvas, contexte, click, appuie = false;
+	var pagejeu = document.getElementById("page-jeu").innerHTML;
 
-    return function (actionNavigationConnexion) {
+	return function (actionNavigationConnexion) {
 
-        this.afficher = function () {
-            document.getElementById("contenu").innerHTML = pagejeu;
-            initialiser()
-        }
-    }
+        	this.afficher = function () {
+	            document.getElementById("contenu").innerHTML = pagejeu;
+        	    initialiser()
+        	}
+	}
 
-    function initialiser() {
-        canvas = document.getElementById("canvas-jeu");
-        document.body.addEventListener('touchmove', function (e) {
-            e.preventDefault();
-        }, false);
-        contexte = canvas.getContext("2d");
 
-        canvas.addEventListener("mousemove", function (e) {
-            recupererPosition('deplacement', e)
-        }, false);
+	function initialiser(){
+		canvas = document.getElementById("canvas-jeu");
 
-        canvas.addEventListener("touchstart", function (e) {
-            //e.preventDefault();
-            recupererPosition('toucher_on', e)
-        }, false);
+		stage = new createjs.Stage(canvas);
+		stage.autoClear = false;
+		stage.enableDOMEvents(true);
 
-        canvas.addEventListener("touchstop", function (e) {
-            //e.preventDefault();
-            recupererPosition('toucher_off', e)
-        }, false);
+		createjs.Touch.enable(stage);
+		createjs.Ticker.framerate = 24;
 
-        canvas.addEventListener("touchmove", function (e) {
-            e.preventDefault();
-            recupererPosition('toucher_deplacement', e)
-        }, false);
-        var t = setInterval(convertirEnImage, 50);
+		canvasDeDessin = new createjs.Shape();
 
-    }
+		stage.addEventListener("stagemousedown", handleMouseDown);
+		stage.addEventListener("stagemouseup", handleMouseUp);
 
-    function recupererPosition(evenement, e) {
-        if (evenement == "deplacement") {
-            ancienX = nouveauX;
-            ancienY = nouveauY;
-            nouveauX = e.clientX - canvas.offsetLeft;
-            nouveauY = e.clientY - canvas.offsetTop;
-            dessiner();
-        }
+		titre = new createjs.Text("Cliquer pour dessiner", "36px Arial", "#777777");
+		titre.x = 0;
+		titre.y = 0;
+		stage.addChild(titre);
 
-        if (evenement = "toucher_on") {
-            appuie = true;
-            //nouveauX = e.touches[0].clientX;// - canvas.offsetLeft;
-            //nouveauY = e.touches[0].clientY;// - canvas.offsetTop;
-        }
+		stage.addChild(canvasDeDessin);
+		stage.update();
+		var t = setInterval(convertirEnImage, 50);
 
-        if (evenement = "toucher_off") {
-            console.log("toucher off");
-            appuie = false;
-        }
+	}
 
-        if (evenement = "toucher_deplacement")
-            ancienX = nouveauX;
-        ancienY = nouveauY;
-        nouveauX = e.touches[0].clientX - canvas.offsetLeft;
-        nouveauY = e.touches[0].clientY - canvas.offsetTop;
-        dessiner();
-    }
 
-    function dessiner() {
-        console.log("" + ancienX + ancienY + nouveauX + nouveauY);
-        contexte.beginPath();
-        contexte.moveTo(ancienX, ancienY);
-        contexte.lineTo(nouveauX, nouveauY);
-        contexte.strokeStyle = 2;
-        contexte.lineWidth = "blue";
-        contexte.stroke();
-        contexte.closePath();
-        //convertirEnImage();
-    }
+	function handleMouseDown(event) {
+		if (!event.primary) { return; }
+		if (stage.contains(titre)) {
+			stage.clear();
+			stage.removeChild(titre);
+		}
+		color = "#000000";
+		stroke = 2;
+		oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
+		oldMidPt = oldPt.clone();
+		stage.addEventListener("stagemousemove", handleMouseMove);
+	}
 
-    function convertirEnImage() {
-        var dataURL = canvas.toDataURL();
-        //document.getElementById("image").innerHTML = dataURL;
-        envoyerImage(dataURL);
-    }
+	function handleMouseMove(event) {
+		if (!event.primary) { return; }
+		var midPt = new createjs.Point(oldPt.x + stage.mouseX >> 1, oldPt.y + stage.mouseY >> 1);
 
-    function envoyerImage(image) {
-        var xmlHttp = new XMLHttpRequest();
-        var url = API_URL + "/image/envoie";
-        xmlHttp.open("POST", url);
-        xmlHttp.setRequestHeader("Content-Type", "application/json");
-        //xmlHttp.setRequestHeader("cache-control", "no-cache");
+		canvasDeDessin.graphics.clear().setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
 
-        var stringAEnvoyer = '{ "image" : "' + image + '"}';
+		oldPt.x = stage.mouseX;
+		oldPt.y = stage.mouseY;
 
-        xmlHttp.send(stringAEnvoyer);
+		oldMidPt.x = midPt.x;
+		oldMidPt.y = midPt.y;
 
-    }
+		stage.update();
+	}
+
+	function handleMouseUp(event) {
+		if (!event.primary) { return; }
+		stage.removeEventListener("stagemousemove", handleMouseMove);
+	}
+
+	function convertirEnImage() {
+		console.log("convert");
+        	var dataURL = canvas.toDataURL();
+	        //document.getElementById("image").innerHTML = dataURL;
+        	envoyerImage(dataURL);
+	}
+
+    	function envoyerImage(image) {
+	        var xmlHttp = new XMLHttpRequest();
+        	var url = API_URL + "/image/envoie";
+	        xmlHttp.open("POST", url);
+        	xmlHttp.setRequestHeader("Content-Type", "application/json");
+	        //xmlHttp.setRequestHeader("cache-control", "no-cache");
+
+        	var stringAEnvoyer = '{ "image" : "' + image + '"}';
+
+	        xmlHttp.send(stringAEnvoyer);
+    	}
 
 })();
